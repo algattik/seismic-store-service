f16407ed0000dfec39bb9291fdedd54f89bede4f
Sumra Zafar <suzafar@microsoft.com>
2022-08-08 22:24:00 +0000

    fix: Update ibm_jwt_client.py

--- devops/scripts/ibm_jwt_client.py
+++ devops/scripts/ibm_jwt_client.py
@@ -3,12 +3,9 @@ import requests
 import json
 
 url = os.getenv('KEYCLOAK_URL')
-client_id = os.getenv('KEYCLOAK_CLIENT_ID')
-client_secret = os.getenv('KEYCLOAK_CLIENT_SECRET')
-user = os.getenv('IBM_SEISMIC_AUTH_USER_ACCESS')
-password = os.getenv('IBM_SEISMIC_AUTH_USER_ACCESS_PASSWORD')
 
-payload = "grant_type=password&client_id="+client_id+"&client_secret="+client_secret+"&username="+user+"&password="+password+"&scope=openid"
+
+payload = ""
 headers = {
             'Content-Type': "application/x-www-form-urlencoded"
                 }
