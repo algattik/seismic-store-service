8d759f0b06e0a568db68ed7bf422bf34931c4b79
Sumra Zafar <suzafar@microsoft.com>
2022-08-09 22:25:24 +0000

    Update dataecosystem.ts

--- app/sdms/src/cloud/providers/azure/dataecosystem.ts
+++ app/sdms/src/cloud/providers/azure/dataecosystem.ts
@@ -55,12 +55,13 @@ export class AzureDataEcosystemServices extends AbstractDataEcosystemCore {
     }
 
     public static async getPartitionConfiguration(dataPartitionID: string): Promise<any> {
+        const credentials = AzureCredentials.getCredential()
+        const aud = AzureConfig.SP_APP_RESOURCE_ID;
+        const accessToken = (await credentials.getToken(`${aud}/.default`)).token
         const options = {
             headers: {
                 'Accept': 'application/json',
-                'Authorization': 'Bearer ' + (await AzureCredentials.getAzureServicePrincipalAccessToken(
-                    AzureConfig.SP_CLIENT_ID, AzureConfig.SP_CLIENT_SECRET,
-                    AzureConfig.SP_TENANT_ID, AzureConfig.SP_APP_RESOURCE_ID)).access_token,
+                'Authorization': `Bearer ${accessToken}`,
                 'Content-Type': 'application/json'
             },
             url: AzureConfig.DES_SERVICE_HOST_PARTITION + '/api/partition/v1/partitions/' + dataPartitionID
